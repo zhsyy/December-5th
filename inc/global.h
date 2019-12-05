5db83b738b9e81c74020753f24d05facd0a99229
fdss-song <37619372+fdss-song@users.noreply.github.com>
2019-12-05 12:39:14 +0800

    modify struct window_t

--- inc/global.h
+++ inc/global.h
@@ -17,6 +17,41 @@
 #define TRUE 1
 #define FALSE 0
 
+#define SEQMAX 0xffffffff
+
+typedef struct {
+	uint32_t last_seq_received; /* 与原来表示内容不同，此处相当于接收方的窗口base，表示当前希望收到的序列号 */
+	uint32_t last_ack_received; /* 最近一次被确认的序列号，相当于发送方的窗口base */
+	pthread_mutex_t ack_lock; /* 单线程的话不需要 */
+	sent_pkt *sent_head; /* 带头结点，指的是如果链表为空，head->next=NULL */
+	uint32_t sent_length; /* 发送但未被确认的数据字节数，nextseqnum = sent_length + last_ack_received */
+	recv_pkt *recv_head;
+	uint32_t recv_length; /* 缓存的数据长度 */
+	pthread_mutex_t recv_lock; /* TCP收到数据，放进去；cmu_read读数据 */
+	uint32_t rwnd; /* 流量控制 */
+	uint32_t cwnd; /* 拥塞控制 */
+	bool timer_on; /* 计时器是否设置 */
+	 /* TimeoutInterval */
+}slide_window_t;
+
+/* 每一个包的起始地址和发送时间 */
+typedef struct {
+	char *pkt_start; /* 包括头部 */
+	struct timeval sent_time; /* 用来计算RTT，单指第一次发送的时间 */
+	sent_pkt *next;
+}sent_pkt;
+
+/* 缓存乱序到达的包的数据，此链表按照seq排序 */
+typedef struct {
+	char *data_start; /* 只有数据 */
+	uint32_t seq; /* 数据的序列号 */
+	uint32_t data_length; /* 数据长度 */
+	bool adjacent; /* 表示与前面的数据包是否相邻，相邻为true
+					（如果当前接收方正在等待的包，也为true）；
+					考虑到维护的是有序链表，减少计算次数 */
+	/* 因此cmu_read是从第一个true一直读到第一个false */
+	recv_pkt *next;
+}recv_pkt;
 
 typedef struct {
 	uint32_t last_seq_received;
@@ -24,6 +59,19 @@ typedef struct {
 	pthread_mutex_t ack_lock;
 } window_t;
 
+typedef enum {
+	CLOSED, /* 0 */
+  LISTEN, /* 1 */
+  SYN_SENT, /* 2 */
+  SYN_RECVD, /* 3 */
+  ESTABLISHED, /* 4 */
+	FIN_WAIT_1, /* 5 */
+	FIN_WAIT_2, /* 6 */
+	CLOSING, /* 7 */
+	TIME_WAIT, /* 8 */
+	CLOSE_WAIT, /* 9 */
+	LAST_ACK /* 10 */
+}states;
 
 typedef struct {
 	int socket;   
@@ -41,7 +89,10 @@ typedef struct {
 	pthread_mutex_t send_lock;
 	int dying;
 	pthread_mutex_t death_lock;
-	window_t window;
+	slide_window_t window;
+	enum states state;
+	uint32_t ISN;
+	uint32_t FSN;
 } cmu_socket_t;
 
 #endif
